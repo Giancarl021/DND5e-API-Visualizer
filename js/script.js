be330a04324437fedcf3b4e6f0913262432bc8c0
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-03-06 00:00:27 -0300

    Added first layer parsre

--- js/script.js
+++ js/script.js
@@ -1,25 +1,42 @@
-const apiUrl = 'http://www.dnd5eapi.co/api/';
+const apiUrl = 'http://www.dnd5eapi.co';
 
 const colors = [
-  '#FF2B67',
-  '#9C96EB',
-  '#37FFD3',
-  '#B2E827',
-  '#FFD79C'
+    '#FF2B67',
+    '#9C96EB',
+    '#37FFD3',
+    '#B2E827',
+    '#FFD79C'
 ];
 
-async function getData() {
-    const response = await fetch(apiUrl);
-    const json = await response.json();
-    return json;
+async function get(url) {
+    return await (await fetch(url)).json();
 }
 
-getData().then(console.log);
-
-
-function init() {
+async function init() {
     const index = Math.floor(Math.random() * colors.length);
     document.documentElement.style.setProperty('--color', colors[index]);
+    const urls = await get(apiUrl + '/api');
+    const size = ((obj) => {
+        let i = 0;
+        for (const key in obj) if (obj.hasOwnProperty(key)) i++;
+        return i;
+    })(urls);
+    for (const key in urls) {
+        if (urls.hasOwnProperty(key)) {
+            parseItem(urls[key]).catch(console.log);
+        }
+    }
+}
+
+async function parseItem(url) {
+    const item = document.createElement('div');
+    item.className = 'item';
+    console.log(url);
+    const title = url.split('/').pop().split('-').map(word => {
+        return word.slice(0, 1).toUpperCase() + word.slice(1);
+    }).join(' ');
+    item.innerText = title;
+    document.querySelector('section').appendChild(item);
 }
 
 document.addEventListener('DOMContentLoaded', init);
