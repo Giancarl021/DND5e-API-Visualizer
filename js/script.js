01160d6b3819d3c5103ad658379634e349fdcef9
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-03-05 23:26:35 -0300

    Frescura added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 const apiUrl = 'http://www.dnd5eapi.co/api/';
 
+const colors = [
+  '#FF2B67',
+  '#9C96EB',
+  '#37FFD3',
+  '#B2E827',
+  '#FFD79C'
+];
+
 async function getData() {
     const response = await fetch(apiUrl);
     const json = await response.json();
@@ -7,3 +15,11 @@ async function getData() {
 }
 
 getData().then(console.log);
+
+
+function init() {
+    const index = Math.floor(Math.random() * colors.length);
+    document.documentElement.style.setProperty('--color', colors[index]);
+}
+
+document.addEventListener('DOMContentLoaded', init);
